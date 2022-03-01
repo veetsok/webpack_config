5fb93bee025b01eb6c405692951006f8f53602bf
Ivan <ivan.kosteev@yandex.ru>
2022-03-02 00:36:08 +0300

    add babel project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const elvenShieldRecipe = {
+  leatherStrips: 2,
+  ironIngnot: 1,
+  refinedMoonstone: 4,
+};
+
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+};
+
+console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
